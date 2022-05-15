044b3666709c61ec172256fdf97c6424e34e8a26
ThiagoCingolani <102705991+ThiagoCingolani@users.noreply.github.com>
2022-05-15 20:58:10 +0200

    Add files via upload

---
+++ Clase 2, 06.04.2022/Ejercicio1.ino
@@ -0,0 +1,14 @@
+int Led_Pin = 8;
+
+void setup()
+{
+  pinMode(Led_Pin, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(Led_Pin, HIGH);
+  delay(1000);
+  digitalWrite(Led_Pin, LOW);
+  delay(1000);
+}
