abf9d4ecd5549b220ca33397118420926d4c8845
ThiagoCingolani <102705991+ThiagoCingolani@users.noreply.github.com>
2022-07-02 02:07:53 +0200

    Add files via upload

---
+++ Clase 8, 01.06.2022/Ejercicio2.ino
@@ -0,0 +1,44 @@
+#include <IRremote.h>
+
+int RECV_PIN = 14;
+int IRData = 0;
+int Led_Pin = 8;
+int Led_Status = 0;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(Led_Pin, OUTPUT);
+  irrecv.enableIRIn();
+}
+
+void loop()
+{
+  if (irrecv.decode(&results))
+  {
+    IRData = results.value;
+    irrecv.resume();
+    Serial.print("Datos: ");
+    Serial.print(IRData, HEX);
+    Serial.print(" {");
+    Serial.print(IRData);
+    Serial.println("}");
+    
+    if (IRData == 255)
+    {
+      if (Led_Status == 0)
+      {
+        digitalWrite(Led_Pin, 1);
+        Led_Status = 1;
+      }
+      else
+      {
+        digitalWrite(Led_Pin, 0);
+        Led_Status = 0;
+      }
+    }
+  }
+}
